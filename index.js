e84f8c533da7e80a740a344e91b59d15337c95b5
Saif Ansari <saifffansari00@gmail.com>
2024-09-12 20:57:18 +0530

    Created profile page

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
+const path = require("path");
+const multer = require("multer");
+const crypto = require("crypto");
 
 const uri = "mongodb://127.0.0.1:27017/socialmedia";
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
@@ -74,6 +78,19 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/");
+});
+
+app.get("/profile", loggedIn, async (req, res) => {
+  let username = req.user.username;
+  await mongoose.connect(uri);
+  let user = await User.findOne({ username }).populate("posts");
+  await mongoose.connection.close();
+  res.render("profile", { user });
+});
+
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
   mongoose.connect(uri).then(() => {
@@ -93,5 +110,91 @@ app.post("/login", (req, res) => {
     });
   });
 });
+
+app.get("/post", loggedIn, (req, res) => {
+  res.render("post");
+});
+
+app.post("/post", loggedIn, async (req, res) => {
+  let { username } = req.user;
+  await mongoose.connect(uri);
+  let user = await User.findOne({ username });
+  let post = await Post.create({
+    user: user._id,
+    post: req.body.post,
+  });
+  user.posts.push(post._id);
+  await user.save();
+  await mongoose.connection.close();
+  res.redirect("/profile");
+});
+
+app.get("/like/:id", loggedIn, async (req, res) => {
+  let { username } = req.user;
+  await mongoose.connect(uri);
+  let user = await User.findOne({ username });
+  let post = await Post.findOne({ _id: req.params.id });
+  if (post.likes.includes(user._id)) {
+    post.likes.splice(post.likes.indexOf(user._id), 1);
+  } else {
+    post.likes.push(user._id);
+  }
+  await post.save();
+  await mongoose.connection.close();
+  res.redirect("/profile");
+});
+
+app.get("/edit/:id", loggedIn, async (req, res) => {
+  await mongoose.connect(uri);
+  let post = await Post.findOne({ _id: req.params.id });
+  await mongoose.connection.close();
+  res.render("edit", { post });
+});
+
+app.post("/edit/:id", loggedIn, async (req, res) => {
+  await mongoose.connect(uri);
+  let post = await Post.findOne({ _id: req.params.id });
+  post.post = req.body.post;
+  await post.save();
+  await mongoose.connection.close();
+  res.redirect("/profile");
+});
+
+app.get("/profile/edit", loggedIn, async (req, res) => {
+  await mongoose.connect(uri);
+  let user = await User.findOne({ username: req.user.username });
+  mongoose.connection.close();
+  res.render("editprofile", { user });
+});
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/images");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(10, (err, bytes) => {
+      let fn = bytes.toString("hex") + path.extname(file.originalname);
+      cb(null, fn);
+    });
+  },
+});
+const upload = multer({ storage });
+
+app.post(
+  "/profile/edit",
+  loggedIn,
+  upload.single("image"),
+  async (req, res) => {
+    await mongoose.connect(uri);
+    let user = await User.findOneAndUpdate({ username: req.user.username });
+    if (req.file) {
+      user.profilepic = req.file.filename;
+    }
+    user.name = req.body.name;
+    await user.save();
+    await mongoose.connection.close();
+    res.redirect("/profile");
+  }
+);
 
 app.listen(3000);
