4093782d17ac29907868961c1558c55ad4d660b7
Saif Ansari <saifffansari00@gmail.com>
2026-02-02 21:42:14 +0530

    Deployment ready

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config(); // Load env variables
 const express = require("express");
 const app = express();
 const User = require("./models/user");
@@ -11,116 +12,57 @@ const multer = require("multer");
 const crypto = require("crypto");
 const fs = require("fs");
 
-const uri = "mongodb://127.0.0.1:27017/socialmedia";
+// Environment variables
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
 app.use(cookieParser());
 
-const loggedIn = (req, res, next) => {
-  let token = req.cookies.token;
-  if (token) {
-    try {
-      let data = jwt.verify(token, "SOCIAL");
-      req.user = data;
-      next();
-    } catch (err) {
-      console.log("Invalid or expired token", err.message);
-      res.redirect("/login");
-    }
-  } else {
-    res.redirect("/login");
+// Database connection function
+async function connectDB() {
+  if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB Connected Successfully");
+  }
+}
+
+// Authentication middleware
+const loggedIn = async (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) return res.redirect("/login");
+
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.user = data;
+    next();
+  } catch (err) {
+    console.error("Invalid or expired token:", err.message);
+    return res.redirect("/login");
   }
 };
 
+// Helper functions
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
-    const randomIndex = Math.floor(Math.random() * (i + 1)); // Get a random index
-    [array[i], array[randomIndex]] = [array[randomIndex], array[i]]; // Swap elements
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    [array[i], array[randomIndex]] = [array[randomIndex], array[i]];
   }
   return array;
 }
 
-app.get("/", loggedIn, async (req, res) => {
-  await mongoose.connect(uri);
-  let user = await User.findOne({ username: req.user.username });
-  let posts = await Post.find().populate("user");
-  await mongoose.connection.close();
-  res.render("index", {
-    user,
-    posts,
-    time: timeAgo,
-    shuffleArray,
-    getFileType,
-  });
-});
-
-app.get("/register", (req, res) => {
-  res.render("register");
-});
-
-app.post("/register", (req, res) => {
-  const { name, username, password, email } = req.body;
-  mongoose.connect(uri).then(async () => {
-    console.log("Connected");
-
-    let user = await User.findOne({ username });
-    if (user) {
-      mongoose.connection.close().then(() => console.log("Connection closed"));
-      return res.status(409).send("User already exists");
-    }
-
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        let createdUser = await User.create({
-          name,
-          username,
-          email,
-          password: hash,
-        });
-
-        let token = jwt.sign({ username }, "SOCIAL");
-        res.cookie("token", token);
-
-        mongoose.connection
-          .close()
-          .then(() => console.log("Connection closed"));
-        res.redirect("/");
-      });
-    });
-  });
-});
-
-app.get("/login", (req, res) => {
-  res.render("login");
-});
-
-app.get("/logout", (req, res) => {
-  res.clearCookie("token");
-  res.redirect("/");
-});
-
 function getFileType(filename) {
-  // Extract the file extension
   const fileExtension = filename.split(".").pop().toLowerCase();
-
-  // Define known image and video extensions
   const imageExtensions = ["jpg", "jpeg", "png", "gif", "bmp", "webp", "svg"];
   const videoExtensions = ["mp4", "webm", "ogg", "mov", "avi", "flv", "mkv"];
 
-  // Check if the extension belongs to an image
-  if (imageExtensions.includes(fileExtension)) {
-    return "image";
-  }
-
-  // Check if the extension belongs to a video
-  if (videoExtensions.includes(fileExtension)) {
-    return "video";
-  }
-
-  // If the file type is not recognized
+  if (imageExtensions.includes(fileExtension)) return "image";
+  if (videoExtensions.includes(fileExtension)) return "video";
   return "unknown";
 }
 
@@ -141,157 +83,202 @@ function timeAgo(milliseconds) {
       return `${timePassed} ${interval.label}${timePassed > 1 ? "s" : ""} ago`;
     }
   }
-
-  return "just now"; // When the milliseconds are less than a second
+  return "just now";
 }
 
-app.get("/profile", loggedIn, async (req, res) => {
-  let username = req.user.username;
-  await mongoose.connect(uri);
-  let user = await User.findOne({ username }).populate("posts");
-  await mongoose.connection.close();
-  res.render("profile", { user, time: timeAgo, getFileType });
-});
+// Routes
 
-app.post("/login", (req, res) => {
-  const { username, password } = req.body;
-  mongoose.connect(uri).then(() => {
-    User.findOne({ username }).then((user) => {
-      mongoose.connection.close().then(() => console.log("Closed connection"));
-      if (!user) {
-        return res.status(404).send("User not found");
-      }
-      bcrypt.compare(password, user.password).then((result) => {
-        if (result) {
-          let token = jwt.sign({ username }, "SOCIAL");
-          res.cookie("token", token);
-          return res.redirect("/");
-        }
-        res.status(401).send("Incorrect Password");
-      });
-    });
+// Home page
+app.get("/", loggedIn, async (req, res) => {
+  await connectDB();
+  const user = await User.findOne({ username: req.user.username });
+  const posts = await Post.find().populate("user");
+  res.render("index", {
+    user,
+    posts,
+    time: timeAgo,
+    shuffleArray,
+    getFileType,
   });
 });
 
-app.get("/post", loggedIn, (req, res) => {
-  res.render("post");
+// Register page
+app.get("/register", (req, res) => res.render("register"));
+
+app.post("/register", async (req, res) => {
+  const { name, username, password, email } = req.body;
+  await connectDB();
+
+  const existingUser = await User.findOne({ username });
+  if (existingUser) return res.status(409).send("User already exists");
+
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  const createdUser = await User.create({
+    name,
+    username,
+    email,
+    password: hash,
+  });
+  const token = jwt.sign({ username }, JWT_SECRET);
+  res.cookie("token", token);
+
+  res.redirect("/");
 });
 
+// Login page
+app.get("/login", (req, res) => res.render("login"));
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  await connectDB();
+
+  const user = await User.findOne({ username });
+  if (!user) return res.status(404).send("User not found");
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(401).send("Incorrect Password");
+
+  const token = jwt.sign({ username }, JWT_SECRET);
+  res.cookie("token", token);
+  res.redirect("/");
+});
+
+// Logout
+app.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/login");
+});
+
+// Profile
+app.get("/profile", loggedIn, async (req, res) => {
+  await connectDB();
+  const user = await User.findOne({ username: req.user.username }).populate(
+    "posts",
+  );
+  res.render("profile", { user, time: timeAgo, getFileType });
+});
+
+// Remove profile picture
 app.get("/remove/:img", loggedIn, async (req, res) => {
-  await mongoose.connect(uri);
-  let user = await User.findOne({ username: req.user.username });
+  await connectDB();
+  const user = await User.findOne({ username: req.user.username });
   user.profilepic = "default.png";
   await user.save();
-  await mongoose.connection.close();
-  fs.unlinkSync(`./public/images/profiles/${req.params.img}`);
+
+  const imgPath = `./public/images/profiles/${req.params.img}`;
+  if (fs.existsSync(imgPath)) fs.unlinkSync(imgPath);
+
   res.redirect("/profile");
 });
+
+// Multer storage for posts
 const postStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/posts");
-  },
-  filename: function (req, file, cb) {
+  destination: "./public/posts",
+  filename: (req, file, cb) => {
     crypto.randomBytes(10, (err, bytes) => {
-      let fn = bytes.toString("hex") + path.extname(file.originalname);
-      cb(null, fn);
+      if (err) return cb(err);
+      cb(null, bytes.toString("hex") + path.extname(file.originalname));
     });
   },
 });
+
+// Create post
+app.get("/post", loggedIn, (req, res) => res.render("post"));
 
 app.post(
   "/post",
   loggedIn,
   multer({ storage: postStorage }).single("media"),
   async (req, res) => {
-    let { username } = req.user;
-    await mongoose.connect(uri);
-    let user = await User.findOne({ username });
-    let post = await Post.create({
+    await connectDB();
+    const user = await User.findOne({ username: req.user.username });
+
+    const post = await Post.create({
       user: user._id,
       post: req.body.post,
       media: req.file ? req.file.filename : "",
     });
+
     user.posts.push(post._id);
     await user.save();
-    await mongoose.connection.close();
     res.redirect("/profile");
-  }
+  },
 );
 
+// Like post
 app.get("/like/:id", loggedIn, async (req, res) => {
-  let { username } = req.user;
-  await mongoose.connect(uri);
-  let user = await User.findOne({ username });
-  let post = await Post.findOne({ _id: req.params.id });
-  if (post.likes.includes(user._id)) {
-    post.likes.splice(post.likes.indexOf(user._id), 1);
-  } else {
-    post.likes.push(user._id);
-  }
+  await connectDB();
+  const user = await User.findOne({ username: req.user.username });
+  const post = await Post.findById(req.params.id);
+
+  const index = post.likes.indexOf(user._id);
+  if (index > -1) post.likes.splice(index, 1);
+  else post.likes.push(user._id);
+
   await post.save();
-  await mongoose.connection.close();
   res.redirect("/");
 });
 
+// Edit post
 app.get("/edit/:id", loggedIn, async (req, res) => {
-  await mongoose.connect(uri);
-  let post = await Post.findOne({ _id: req.params.id });
-  await mongoose.connection.close();
+  await connectDB();
+  const post = await Post.findById(req.params.id);
   res.render("edit", { post });
 });
 
 app.post("/edit/:id", loggedIn, async (req, res) => {
-  await mongoose.connect(uri);
-  let post = await Post.findOne({ _id: req.params.id });
+  await connectDB();
+  const post = await Post.findById(req.params.id);
   post.post = req.body.post;
   await post.save();
-  await mongoose.connection.close();
   res.redirect("/profile");
 });
 
+// Edit profile
 app.get("/profile/edit", loggedIn, async (req, res) => {
-  await mongoose.connect(uri);
-  let user = await User.findOne({ username: req.user.username });
-  await mongoose.connection.close();
+  await connectDB();
+  const user = await User.findOne({ username: req.user.username });
   res.render("editprofile", { user });
 });
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./public/images/profiles");
-  },
-  filename: function (req, file, cb) {
+const profileStorage = multer.diskStorage({
+  destination: "./public/images/profiles",
+  filename: (req, file, cb) => {
     crypto.randomBytes(10, (err, bytes) => {
-      let fn = bytes.toString("hex") + path.extname(file.originalname);
-      cb(null, fn);
+      if (err) return cb(err);
+      cb(null, bytes.toString("hex") + path.extname(file.originalname));
     });
   },
 });
-const upload = multer({ storage });
+const upload = multer({ storage: profileStorage });
 
 app.post(
   "/profile/edit",
   loggedIn,
   upload.single("image"),
   async (req, res) => {
-    await mongoose.connect(uri);
-    let user = await User.findOne({ username: req.user.username });
-    if (req.file) {
-      user.profilepic = req.file.filename;
-    }
+    await connectDB();
+    const user = await User.findOne({ username: req.user.username });
+
+    if (req.file) user.profilepic = req.file.filename;
     user.name = req.body.name;
+
     await user.save();
-    await mongoose.connection.close();
     res.redirect("/profile");
-  }
+  },
 );
 
+// View other user
 app.get("/user/:id", loggedIn, async (req, res) => {
-  await mongoose.connect(uri);
-  let user = await User.findOne({ _id: req.params.id }).populate("posts");
-  let loggedUser = await User.findOne({ username: req.user.username });
-  await mongoose.connection.close();
+  await connectDB();
+  const user = await User.findById(req.params.id).populate("posts");
+  const loggedUser = await User.findOne({ username: req.user.username });
   res.render("user", { user, time: timeAgo, loggedUser, getFileType });
 });
 
-app.listen(3000);
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
